fba9591998af61c3a479ebd9c751e346ac514181
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-12 19:29:40 +0000

    Deploy 42tg/weapon_calculator_web to github.com/42tg/weapon_calculator_web.git:gh-pages

---
+++ precache-manifest.a4a3c569197f6b09e024c5ddf3f1525d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0aa94cbff6a1479f9536",
+    "url": "/weapon_calculator_web/static/js/runtime~main.0aa94cbf.js"
+  },
+  {
+    "revision": "2d3c5e8d984701cb7da7",
+    "url": "/weapon_calculator_web/static/js/main.2d3c5e8d.chunk.js"
+  },
+  {
+    "revision": "99b2f1023a25fe02aaf4",
+    "url": "/weapon_calculator_web/static/js/2.99b2f102.chunk.js"
+  },
+  {
+    "revision": "99b2f1023a25fe02aaf4",
+    "url": "/weapon_calculator_web/static/css/2.ef20079d.chunk.css"
+  },
+  {
+    "revision": "9b8e71c35dbbc866c129e94c7cc6b213",
+    "url": "/weapon_calculator_web/index.html"
+  }
+];
